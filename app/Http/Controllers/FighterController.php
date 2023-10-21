6aa0b2e39d4d57fddfdbb17d01ff9c64ae68bee4
kingkazuma7 <your.nasionasio7@gmail.com>
2023-10-21 11:20:23 +0900

    選手コントローラー作成

---
+++ app/Http/Controllers/FighterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fighter;
+use Illuminate\Http\Request;
+
+class FighterController extends Controller
+{
+    // createメソッド
+    public function create()
+    {
+        return view('fighters.create');
+    }
+    
+    // store メソッド ... db保存
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'entry_song' => 'required',
+            'artist' => 'required',
+        ]);
+        
+        Fighter::create($request->all());
+        
+        return redirect()->route('fighters.index')
+            ->with('success', 'ファイターが登録されました。');
+    }
+}
